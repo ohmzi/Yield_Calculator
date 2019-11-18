e0a0108a5d2bdb2be7e4855fd29fb8b2a3ef519e
Omar <omariqbal97@gmail.com>
2019-11-18 00:41:52 -0500

    debugging IF checks 4

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Calculate that is specifically designed to project few values based on stock current value, dividend, dividend %+-, stock value % over period of time.
 
 
+debugging IF checks 4:
+reinvestment working OK.
+
 Version 2:
 Dark Mode
 Calcualtion corrected
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -18,16 +18,16 @@ export class Tab2Page {
   perctGainLoss: string = "dark";
   yearString = "Number of years";
   maxYears: boolean = false;
-  dividendGainLossNegative: boolean = false;
 
   //input
   cash: number;
   stockPrice: number;
   sharePerctExternal: number;
+  stockNegative: boolean = false;
   numYear: number;
   dividendPerctExternal: number;
   dividendGainLossExternal: number;
-  stockNegative: boolean = false;
+  dividendGainLossNegative: boolean = false;
 
   //output
   netCashed: number;
@@ -39,7 +39,7 @@ export class Tab2Page {
   dividendGainLossInternal: number;
   sharePerctInternal: number;
   numStocks: number;
-  dividentReturnsPerStock: number = 0;
+  dividentReturnsPerStock: number;
   remainingCash: number;
   investedCashInit: number;
   dividendPerctInternal: number;
@@ -48,224 +48,258 @@ export class Tab2Page {
   totalPoolOfMoney: number;
   frozenNumStocks: number;
   numStocksPurchasedPerQuarter: number;
-  monthWhenNewStockBought:number;
-  
+  monthWhenNewStockBought: number;
+
   constructor() {}
 
-  calculate() {
-    this.intitalCheck();
-    if (this.numYear > 50) {
-      this.maxYears = true;
+  private calculate() {
+    console.log("---------calculate---------");
+
+    if (this.numYear > 50 || this.numYear <= 0) {
+      //Number of years are not between 1-50
       this.clearEverything();
-      this.yearString = "Please reduce number of years of under 50";
+      this.numYearIssue();
     } else {
-      this.maxYears = false;
-      this.shareValue = this.stockPrice;
-      this.totalPoolOfMoney = this.cash;
-      this.frozenNumStocks = Math.floor(
-        this.totalPoolOfMoney / this.stockPrice
-      );
-
-      console.log("frozenNumStocks", this.frozenNumStocks);
+      //transfering external values to internal variables
+      this.intitalCheck();
       if (this.frozenNumStocks >= 1) {
-        if (this.numYear <= 0) {
-          console.log("Years have a problem!");
-          this.clearEverything();
-        } else {
-          //converting from percent to numeric
-          this.dividendPerctInternal = this.dividendPerctExternal / 100;
-          console.log("Divident Percentage: ", this.dividendPerctInternal);
-
-          //year loop
-          for (var yearLoop= 0; yearLoop< this.numYear; yearLoop++) {
-            console.log("Loop of year: ", yearLoop);
-
-            console.log("totalPoolOfMoney ", this.totalPoolOfMoney);
-            this.numStocks = Math.floor(
-              this.totalPoolOfMoney / this.stockPrice
-            );
-            console.log("Number of stocks: ", this.numStocks);
-            this.investedCashInit = this.numStocks * this.stockPrice;
-            console.log("Cash invested: ", this.investedCashInit);
-            this.remainingCash = this.totalPoolOfMoney - this.investedCashInit;
-            console.log("Cash Remaining: ", this.remainingCash);
-
-            //////////////////////////////////////////////////////////////////
-            // Quarterly loop
-            this.shareValue2=this.shareValue/4;
-            for (var quarterLoop= 3; quarterLoop<= 12; quarterLoop= quarterLoop+ 3) {
-              console.log(
-                "Divident return Per Share end of quarter",
-                "year: ",
-                yearLoop,
-                "month: ",
-                quarterLoop/ 3,
-                ": ",
-                this.dividentReturnsPerStock
-              );
-              if (this.dividentReturnsPerStock >= this.shareValue) {
-                this.numStocksPurchasedPerQuarter = Math.floor(
-                  this.dividentReturnsPerStock / this.shareValue
-                );
-        
-                console.log(
-                  "Number of stocks purchased in quarter ",
-                  quarterLoop,
-                  ": ",
-                  this.numStocksPurchasedPerQuarter
-                );
-
-                this.numStocks =
-                  this.numStocks + this.numStocksPurchasedPerQuarter;
-                this.numStocksPurchasedPerQuarter = 0;
-                this.dividentReturnsPerStock =
-                  this.dividentReturnsPerStock - this.shareValue;
-
-                /*   console.log(
-                  "Divident return Per Share end of quarter",
-                  i,
-                  ": ",
-                  this.dividentReturnsPerStock
-                );
-                */
-              }
-              this.dividentReturnsPerStock =
-                this.dividentReturnsPerStock +
-                this.dividendPerctInternal * this.stockPrice;
-              console.log(
-                "Divident is still postive, Dividend value: ",
-                this.dividentReturnsPerStock
-              );
-              this.investedCashInit =
-              Math.round(
-                (this.shareValue2 + this.dividentReturnsPerStock) *
-                  this.numStocks *
-                  100
-              ) / 100;
-            }
-
-            this.something =
-              this.dividendPerctInternal *
-              (1 - this.dividendGainLossInternal / 100);
-
-            console.log("Something: ", this.something);
-
-            ////
-
-            this.dividendPerctInternal =
-              this.dividendPerctInternal + this.dividendGainLossInternal;
-            if (this.dividendPerctInternal < 0) {
-              this.dividendPerctInternal = 0;
-            }
-            console.log(
-              "Divident this year will be, Dividend value: ",
-              this.dividentReturnsPerStock
-            );
-
-            //dividendGainLossInternal
-            ////
-
-            console.log(
-              "Divident Percentage % in year ",
-              yearLoop,
-              " ",
-              this.dividendPerctInternal
-            );
-            this.shareValue =
-              this.shareValue * (1 + this.sharePerctInternal / 100);
-              console.log(
-                "Stocks value changed in year: ",
-                quarterLoop,
-                "to : ",
-                this.shareValue
-              );
-          
-            console.log(
-              "Share Value changed over year: ",
-              yearLoop,
-              ": ",
-              this.shareValue
-            );
-            console.log(
-              "Money that you invested has become: ",
-              this.investedCashInit
-            );
-            this.totalPoolOfMoney =
-              Math.round((this.remainingCash + this.investedCashInit) * 100) /
-              100;
-            console.log("NET CASH: ", this.totalPoolOfMoney);
-            console.log("totalPoolOfMoney ", this.totalPoolOfMoney);
-
-            //////////////////////////////////////////////////////////////////
-          }
-          console.log("totalPoolOfMoney ", this.totalPoolOfMoney);
-
-          this.projReturn =
-            Math.round((this.totalPoolOfMoney - this.cash) * 100) / 100;
-          this.netCashed = this.totalPoolOfMoney;
-          this.newStocks = this.numStocks - this.frozenNumStocks;
-          this.dividentReturnsPerStock = 0;
-        }
+        //yearly Loop does yearly Calculations
+        this.yearlyLoop();
+        //return sents internal values to external view components
+        this.yearlyReturns();
       } else {
-        console.log("PROBLEM!");
-
+        console.log("No stocks purchased!");
         this.clearEverything();
       }
-
-      /////////////////////////////////////////////////
-
-      //theming
-      this.completion = true;
-      if (this.netCashed >= this.cash) {
-        this.newProjSPColour = "success";
-      } else {
-        this.netCashed = 0;
-        this.newProjSPColour = "danger";
-      }
-      if (this.newStocks < 1 || this.newStocks == null) {
-        this.newStocks = 0;
-        this.newStockColour = "dark";
-      } else {
-        this.newStockColour = "success";
-      }
+      this.theming();
     }
   }
 
-  private dividentNegativeMethod() {
+  private yearlyLoop() {
+    console.log("---------yearlyLoop---------");
+
+    for (var yearLoop = 1; yearLoop <= this.numYear; yearLoop++) {
+      console.log("---------Year", yearLoop, "---------");
+      console.log("totalPoolOfMoney: ", this.totalPoolOfMoney);
+      console.log("numStocks: ", this.numStocks);
+      console.log("investedCashInit: ", this.investedCashInit);
+      console.log("remainingCash: ", this.remainingCash);
+      console.log("dividendPerctInternal: ", this.dividendPerctInternal);
+      ///////////////////////////////////////////////////////////////////
+      this.numStocks = Math.floor(this.totalPoolOfMoney / this.stockPrice);
+      this.investedCashInit = this.numStocks * this.stockPrice;
+      this.remainingCash = this.totalPoolOfMoney - this.investedCashInit;
+      this.shareValue2 = this.shareValue / 4;
+      console.log("numStocks: ", this.numStocks);
+      console.log("totalPoolOfMoney: ", this.totalPoolOfMoney);
+      console.log("stockPrice: ", this.stockPrice);
+      console.log("investedCashInit: ", this.investedCashInit);
+      console.log("remainingCash: ", this.remainingCash);
+      console.log("shareValue2: ", this.shareValue2);
+
+      //////////////////////////////////////////////////////////////////
+      this.quarterlyLoop();
+
+      this.annualPertChange();
+
+      console.log(
+        "Money that you invested has become: ",
+        this.investedCashInit
+      );
+      this.totalPoolOfMoney =
+        Math.round((this.remainingCash + this.investedCashInit) * 100) / 100;
+      console.log("totalPoolOfMoney ", this.totalPoolOfMoney);
+      //////////////////////////////////////////////////////////////////
+    }
+  }
+
+  private annualPertChange() {
+    console.log("---------annualPertChange---------");
+
     this.dividendPerctInternal =
-      this.dividendPerctInternal * (1 - this.dividendGainLossInternal / 100);
+      this.dividendPerctInternal + this.dividendGainLossInternal;
     console.log(
-      "Divident is still postive even after minusing, Dividend value: ",
+      "Dividend % changed in year: ",
+      "to : ",
+      this.dividendPerctInternal
+    );
+    this.shareValue = this.shareValue * (1 + this.sharePerctInternal);
+    console.log("Stocks value changed in year: ", "to : ", this.shareValue);
+  }
+
+  private quarterlyLoop() {
+    console.log("---------quarterlyLoop---------");
+
+    for (
+      var quarterLoop = 3;
+      quarterLoop <= 12;
+      quarterLoop = quarterLoop + 3
+    ) {
+      console.log("---------Quarter", quarterLoop / 3, "---------");
+      console.log("Dividend: ", this.dividentReturnsPerStock);
+      this.reinvesting();
+      this.calculateDividend();
+      this.quarterlyResults();
+    }
+  }
+
+  private calculateDividend() {
+    console.log("---------calculateDividend---------");
+    console.log("dividendPerctInternal: ", this.dividendPerctInternal);
+    console.log(
+      "dividentReturnsPerStock Before Calcualting Quarterly: ",
       this.dividentReturnsPerStock
+    );
+    console.log("StockPrice: ", this.stockPrice);
+    this.dividentReturnsPerStock = this.dividendPerctInternal * this.stockPrice;
+    console.log(
+      "dividentReturnsPerStock After Calcualting Quarterly: ",
+      this.dividentReturnsPerStock
+    );
+    if (this.dividentReturnsPerStock < 0) {
+      console.log("Negative Dividend: ", this.dividentReturnsPerStock);
+      this.dividentReturnsPerStock = 0;
+    }
+  }
+
+  private reinvesting() {
+    if (this.dividentReturnsPerStock >= this.shareValue) {
+      console.log("---------Reinvesting---------");
+      this.numStocksPurchasedPerQuarter = Math.floor(
+        this.dividentReturnsPerStock / this.shareValue
+      );
+      console.log("Stocks purchased: ", this.numStocksPurchasedPerQuarter);
+      this.numStocks = this.numStocks + this.numStocksPurchasedPerQuarter;
+      for (var i = this.numStocksPurchasedPerQuarter; (i > 0); i--) {
+        console.log(
+          "For Counter to reduce the cost of new stocks purchased",
+          i
+        );
+        console.log(
+          "numStocksPurchasedPerQuarter: ",
+          this.numStocksPurchasedPerQuarter
+        );
+        console.log("shareValue: ", this.shareValue);
+        console.log("dividentReturnsPerStock: ", this.dividentReturnsPerStock);
+
+        this.dividentReturnsPerStock =
+          this.dividentReturnsPerStock - this.shareValue;
+        console.log("dividentReturnsPerStock: ", this.dividentReturnsPerStock);
+        this.numStocksPurchasedPerQuarter =
+          this.numStocksPurchasedPerQuarter - 1;
+      }
+      console.log(
+        "numStocksPurchasedPerQuarter: ",
+        this.numStocksPurchasedPerQuarter
+      );
+    }
+  }
+
+  private quarterlyResults() {
+    console.log("---------quarterlyResults---------");
+    this.investedCashInit =
+      Math.round(
+        (this.shareValue2 + this.dividentReturnsPerStock) * this.numStocks * 100
+      ) / 100;
+  }
+
+  private yearlyReturns() {
+    console.log("---------yearlyReturns---------");
+
+    console.log("totalPoolOfMoney ", this.totalPoolOfMoney);
+    console.log("numStocks ", this.numStocks);
+    this.projReturn =
+      Math.round((this.totalPoolOfMoney - this.cash) * 100) / 100;
+    this.netCashed = this.totalPoolOfMoney;
+    this.newStocks = this.numStocks - this.frozenNumStocks;
+    this.dividentReturnsPerStock = 0;
+  }
+
+  private intitalCheck() {
+    console.log("---------intitalCheck---------");
+    this.internalValueTransfer();
+    this.isStockNegative();
+    this.isDividendNegative();
+  }
+
+  private internalValueTransfer() {
+    console.log("---------internalValueTransfer---------");
+
+    this.maxYears = false;
+    this.shareValue = this.stockPrice;
+    this.totalPoolOfMoney = this.cash;
+    this.dividendPerctInternal = this.dividendPerctExternal / 100;
+    this.frozenNumStocks = Math.floor(this.totalPoolOfMoney / this.stockPrice);
+    this.dividentReturnsPerStock = 0;
+    this.investedCashInit = 0;
+
+    console.log("totalPoolOfMoney: ", this.totalPoolOfMoney);
+    console.log("shareValue: ", this.shareValue);
+    console.log("sharePerctInternal: ", this.sharePerctInternal);
+    console.log("numYear: ", this.numYear);
+    console.log("dividendPerctInternal: ", this.dividendPerctInternal);
+    console.log("frozenNumStocks: ", this.frozenNumStocks);
+  }
+
+  private isDividendNegative() {
+    console.log("---------isDividendNegative---------");
+    console.log("checkbox: ", this.dividendGainLossNegative);
+
+    if (this.dividendGainLossNegative == true) {
+      this.dividendGainLossColor = "danger";
+      this.dividendGainLossInternal = -this.dividendGainLossExternal / 100;
+    } else {
+      this.dividendGainLossColor = "dark";
+      this.dividendGainLossInternal = this.dividendGainLossExternal;
+    }
+    console.log(
+      "dividendGainLossInternal : ",
+      this.dividendGainLossInternal / 100
     );
   }
 
-  clearEverything() {
+  private isStockNegative() {
+    console.log("---------isStockNegative---------");
+    console.log("checkbox: ", this.stockNegative);
+
+    if (this.stockNegative == true) {
+      this.sharePerctInternal = -this.sharePerctExternal / 100;
+      this.sharePerctColor = "danger";
+    } else {
+      this.sharePerctInternal = this.sharePerctExternal / 100;
+      this.sharePerctColor = "dark";
+    }
+    console.log("sharePerctInternal: ", this.sharePerctInternal);
+  }
+
+  private clearEverything() {
+    console.log("---------clearEverything---------");
     this.netCashed = 0;
     this.projReturn = 0;
     this.newStocks = 0;
     this.dividentReturnsPerStock = 0;
   }
 
-  intitalCheck() {
-    if (this.stockNegative == true) {
-      this.sharePerctInternal = -this.sharePerctExternal;
-      this.sharePerctColor = "danger";
-      console.log("checkbox: ", this.stockNegative);
-    } else {
-      this.sharePerctInternal = this.sharePerctExternal;
-      this.sharePerctColor = "dark";
-      console.log("checkbox: ", this.stockNegative);
-    }
+  private numYearIssue() {
+    console.log("---------numYearIssue---------");
 
-    if (this.dividendGainLossNegative == true) {
-      this.dividendGainLossColor = "danger";
-      this.dividendGainLossInternal = -this.dividendGainLossExternal;
-      console.log("dividendGainLossInternal: ", this.dividendGainLossInternal);
+    this.maxYears = true;
+    this.yearString = "Please enter Years Between 1-50";
+  }
+
+  private theming() {
+    this.completion = true;
+    if (this.netCashed >= this.cash) {
+      this.newProjSPColour = "success";
     } else {
-      this.dividendGainLossColor = "dark";
-      this.dividendGainLossInternal = this.dividendGainLossExternal;
-      console.log("dividendGainLossInternal : ", this.dividendGainLossInternal);
+      this.netCashed = 0;
+      this.newProjSPColour = "danger";
+    }
+    if (this.newStocks < 1 || this.newStocks == null) {
+      this.newStocks = 0;
+      this.newStockColour = "dark";
+    } else {
+      this.newStockColour = "success";
     }
   }
 }
